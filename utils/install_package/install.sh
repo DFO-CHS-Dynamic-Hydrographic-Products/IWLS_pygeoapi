3b22ec63663b24af14e119b3a81b4a8f97c79b30
mcarre <maxime.carre@dfo-mpo.gc.ca>
2022-07-25 11:49:10 -0700

    updated install.sh to support s111

--- utils/install_package/install.sh
+++ utils/install_package/install.sh
@@ -1,6 +1,6 @@
 mkdir ./http_cache
 mkdir ./logs
-mkdir ./s104_process
+mkdir ./s100_process
 pip install -r requirements.txt
 pip install provider_iwls-0.0.1-py3-none-any.whl
 export PYGEOAPI_CONFIG=config_dev.yml
