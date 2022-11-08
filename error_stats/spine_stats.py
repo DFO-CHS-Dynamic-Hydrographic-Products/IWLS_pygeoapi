cfcecc0c6d29d5c0a260600e032e3452cb0a6504
mcarre <maxime.carre@dfo-mpo.gc.ca>
2022-11-08 10:54:08 -0800

    Implemented  daily errors calculation method; Improved doc strings

--- error_stats/spine_stats.py
+++ error_stats/spine_stats.py
@@ -1,6 +1,6 @@
 
+import datetime
 import sys
-from telnetlib import PRAGMA_HEARTBEAT
 sys.path.append('../')
 
 import pandas as pd
@@ -13,6 +13,12 @@ class SpineErrors():
         self.spine_stations = pd.read_csv('spine_stn.csv',dtype = str)
 
     def _get_station_data(self,start_time, end_time):
+        """
+        Request data from IWLS public API for stations with SPINE timeseries for a user defined time frame
+        :param start_time:  Start time, ISO 8601 format UTC (e.g.: 2019-11-13T19:18:00Z) (string)
+        :param end_time:  End time, ISO 8601 format UTC (e.g.: 2019-11-13T19:18:00Z) (string)
+        :returns: dict of pandas.DataFrame
+        """
         codes = self.spine_stations['code'].to_list()
         api = iwls.IwlsApiConnector()
         stns_data = {}
@@ -37,6 +43,13 @@ class SpineErrors():
         return stns_data
 
     def compute_errors(self,start_time, end_time):
+        """
+        Compute  errors between SPINE and WLO for a user
+        defined time frame
+        :param start_time:  Start time, ISO 8601 format UTC (e.g.: 2019-11-13T19:18:00Z) (string)
+        :param end_time:  End time, ISO 8601 format UTC (e.g.: 2019-11-13T19:18:00Z) (string)
+        :returns: pandas.DataFrame
+        """
         # Request data for station with SPINE time series
         stns_data = self._get_station_data(start_time, end_time)
         # Compute diff between wlo and SPINE
@@ -65,6 +78,21 @@ class SpineErrors():
 
         return errors_df
 
-errors = SpineErrors().compute_errors('2022-10-00T00:00:00Z','2022-10-30T23:59:00Z')
-errors.to_csv('spine_test.csv')
-print(errors)
+    def compute_daily_errors(self):
+        """
+        Compute daily errors between SPINE and WLO
+        :returns: csv file
+        """
+        start_time = datetime.date.today() - datetime.timedelta(days=1)
+        end_time = start_time + datetime.timedelta(minutes=1439)
+
+        start_time = start_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+        end_time = end_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+        errors =  self.compute_errors(start_time, end_time)
+        errors.to_csv(f'{start_time}.csv')
+
+
+#errors = SpineErrors().compute_errors('2022-10-00T00:00:00Z','2022-10-30T23:59:00Z')
+#errors.to_csv('spine_test.csv')
+#print(errors)
