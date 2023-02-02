73041c29bef49158c4d6026554e54007a6a6a940
mcarre <maxime.carre@dfo-mpo.gc.ca>
2023-02-02 09:38:38 -0800

    PEP-8 Formatting

--- utils/s100_to_csv.py
+++ utils/s100_to_csv.py
@@ -1,8 +1,12 @@
-import pandas as pd
-import h5py
+# Standard library imports
+import sys
 import datetime
 import dateutil
-import sys
+
+# Packages imports
+import pandas as pd
+import h5py
+
 
 def s100_to_csv(filepath):
     """
@@ -25,9 +29,9 @@ def s100_to_csv(filepath):
         ### S104 Conversion ###
         if group_path == '/WaterLevel/':
             for i in hdf_file['/WaterLevel/']:
-                if i != 'axisName':
+                if i != 'axisNames':
                     data_type = hdf_file[f'/WaterLevel/{i}/'].attrs['typeOfWaterLevelData']
-                    #Format data type
+                    # Format data type
                     if data_type == 2:
                         data_type = 'wlp'
                     elif data_type == 1:
@@ -35,7 +39,7 @@ def s100_to_csv(filepath):
                     else:
                         data_type = 'wlf'
 
-                    #Format data into dataframe with datetime index
+                    # Format data into dataframe with datetime index
                     for group in hdf_file[f'/WaterLevel/{i}/']:
                         if group != 'Positioning':
                             # Get Station label
@@ -47,60 +51,72 @@ def s100_to_csv(filepath):
                             # Return only water Level from (water levels,trends) tuples
                             stn_data = [i[0] for i in stn_data]
                             # Generate Timestamps for index
-                            number_of_times = int(hdf_file[f'/WaterLevel/{i}/{group}/'].attrs['numberOfTimes'])
-                            interval = int(hdf_file[f'/WaterLevel/{i}/{group}/'].attrs['timeRecordInterval'])
+                            number_of_times = int(
+                                hdf_file[f'/WaterLevel/{i}/{group}/'].attrs['numberOfTimes'])
+                            interval = int(
+                                hdf_file[f'/WaterLevel/{i}/{group}/'].attrs['timeRecordInterval'])
                             start_time = hdf_file[f'/WaterLevel/{i}/{group}/'].attrs['startDateTime']
                             start_time = dateutil.parser.parse(start_time)
-                            end_time = start_time + datetime.timedelta(seconds=((number_of_times*interval)-interval))
-                            timestamps = pd.date_range(start_time,end_time, freq = f'{interval}S')
-                            
-                            group_df = pd.DataFrame(stn_data,index=timestamps, columns=[stn_label])
+                            end_time = start_time + \
+                                datetime.timedelta(
+                                    seconds=((number_of_times*interval)-interval))
+                            timestamps = pd.date_range(
+                                start_time, end_time, freq=f'{interval}S')
+
+                            group_df = pd.DataFrame(
+                                stn_data, index=timestamps, columns=[stn_label])
                             if data_df.empty:
                                 data_df = group_df
                             else:
-                                data_df = pd.concat([data_df, group_df], axis=1)
+                                data_df = pd.concat(
+                                    [data_df, group_df], axis=1)
 
-        ### S111 Conversion ###                      
+        ### S111 Conversion ###
         else:
             for group in hdf_file['/SurfaceCurrent/SurfaceCurrent.01/']:
                 if group != 'Positioning':
                     # Get Station label
                     stn_name = hdf_file[f'/SurfaceCurrent/SurfaceCurrent.01/{group}/'].attrs['stationName']
-                    stn_id = hdf_file[f'/SurfaceCurrent/SurfaceCurrent.01/{group}/'].attrs['stationIdentification']
+                    stn_id = hdf_file[
+                        f'/SurfaceCurrent/SurfaceCurrent.01/{group}/'].attrs['stationIdentification']
                     stn_label = f'{stn_id}_{stn_name}'
                     # Get Station data
                     stn_data = hdf_file[f'/SurfaceCurrent/SurfaceCurrent.01/{group}/values'][:]
                     stn_speed = [i[0] for i in stn_data]
                     stn_direction = [i[1] for i in stn_data]
                     # Generate Timestamps for index
-                    number_of_times = int(hdf_file[f'/SurfaceCurrent/SurfaceCurrent.01/{group}/'].attrs['numberOfTimes'])
-                    interval = int(hdf_file[f'/SurfaceCurrent/SurfaceCurrent.01/{group}/'].attrs['timeRecordInterval'])
-                    start_time = hdf_file[f'/SurfaceCurrent/SurfaceCurrent.01/{group}/'].attrs['startDateTime']
+                    number_of_times = int(
+                        hdf_file[f'/SurfaceCurrent/SurfaceCurrent.01/{group}/'].attrs['numberOfTimes'])
+                    interval = int(
+                        hdf_file[f'/SurfaceCurrent/SurfaceCurrent.01/{group}/'].attrs['timeRecordInterval'])
+                    start_time = hdf_file[
+                        f'/SurfaceCurrent/SurfaceCurrent.01/{group}/'].attrs['startDateTime']
                     start_time = dateutil.parser.parse(start_time)
-                    end_time = start_time + datetime.timedelta(seconds=((number_of_times*interval)-interval))
-                    timestamps = pd.date_range(start_time,end_time, freq = f'{interval}S')
+                    end_time = start_time + \
+                        datetime.timedelta(
+                            seconds=((number_of_times*interval)-interval))
+                    timestamps = pd.date_range(
+                        start_time, end_time, freq=f'{interval}S')
 
-                    group_df = pd.DataFrame(stn_speed,index=timestamps, columns=[f'{stn_label}_wcs'])
-                    dir_df = pd.DataFrame(stn_direction,index=timestamps, columns=[f'{stn_label}_wcd'])
+                    group_df = pd.DataFrame(
+                        stn_speed, index=timestamps, columns=[f'{stn_label}_wcs'])
+                    dir_df = pd.DataFrame(stn_direction, index=timestamps, columns=[
+                                          f'{stn_label}_wcd'])
                     group_df = pd.concat([group_df, dir_df], axis=1)
                     if data_df.empty:
                         data_df = group_df
                     else:
                         data_df = pd.concat([data_df, group_df], axis=1)
 
-
-
-
         # Export stations to csv files
         station_list = data_df.columns.values.tolist()
         station_list = list(set([i[0:-4] for i in station_list]))
         for i in station_list:
             station_df = data_df.loc[:, data_df.columns.str.contains(f'{i}')]
-            station_df = station_df.rename(columns = lambda x : str(x)[-3:])
+            station_df = station_df.rename(columns=lambda x: str(x)[-3:])
             station_df.index.name = 'datetime'
             station_df.to_csv(f'{i}.csv')
 
-                
 
 filepath = str(sys.argv[1])
 
